a6e38636c935b39bca0724f8f761e0970bee5bf2
than-dev <nathan.cotrim@gmail.com>
2023-04-13 00:08:21 -0300

    feat: number trivia bloc state

--- lib/features/number_trivia/presentation/bloc/number_trivia/number_trivia_bloc.dart
+++ lib/features/number_trivia/presentation/bloc/number_trivia/number_trivia_bloc.dart
@@ -1,12 +1,12 @@
-import 'package:bloc/bloc.dart';
+import 'package:clean_flutter/features/number_trivia/presentation/bloc/number_trivia/number_trivia_state.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter/foundation.dart';
 
 part 'number_trivia_event.dart';
-part 'number_trivia_state.dart';
 
 class NumberTriviaBloc extends Bloc<NumberTriviaEvent, NumberTriviaState> {
-  NumberTriviaBloc() : super(NumberTriviaInitial()) {
+  NumberTriviaBloc() : super(NumberTriviaEmptyState()) {
     on<NumberTriviaEvent>((event, emit) {
       // TODO: implement event handler
     });
--- lib/features/number_trivia/presentation/bloc/number_trivia/number_trivia_state.dart
+++ lib/features/number_trivia/presentation/bloc/number_trivia/number_trivia_state.dart
@@ -1,10 +1,25 @@
-part of 'number_trivia_bloc.dart';
+import 'package:clean_flutter/features/number_trivia/domain/entities/number_trivia.dart';
+import 'package:equatable/equatable.dart';
 
 abstract class NumberTriviaState extends Equatable {
-  const NumberTriviaState();
-  
+  const NumberTriviaState([List props = const <dynamic>[]]);
+
   @override
   List<Object> get props => [];
 }
 
-class NumberTriviaInitial extends NumberTriviaState {}
+class NumberTriviaEmptyState extends NumberTriviaState {}
+
+class NumberTriviaLoadingState extends NumberTriviaState {}
+
+class NumberTriviaLoadedState extends NumberTriviaState {
+  final NumberTrivia trivia;
+
+  NumberTriviaLoadedState({required this.trivia}) : super([trivia]);
+}
+
+class NumberTriviaErrorState extends NumberTriviaState {
+  final String message;
+
+  NumberTriviaErrorState({required this.message}) : super([message]);
+}
